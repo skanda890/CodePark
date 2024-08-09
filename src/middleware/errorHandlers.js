9564fc6360ff5e0dc5ff87c59c0b7fe66e8789e5
SkandaBT <9980056379Skanda@gmail.com>
2024-08-09 16:46:19 +0530

    Delete src directory
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- src/middleware/errorHandlers.js
+++
@@ -1,8 +0,0 @@
-export const notFoundHandler = (req, res, next) => {
-  res.status(404).send('Resource not found');
-};
-
-export const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-};
