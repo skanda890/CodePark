c550f579cb21801c8dfdcb5a6d7a260d0fecd040
SkandaBT <9980056379Skanda@gmail.com>
2024-08-09 16:39:37 +0530

    Update webpack.config.js
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,34 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { CleanWebpackPlugin } from 'clean-webpack-plugin';
-import { DefinePlugin } from 'webpack';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-export default {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve('dist'),
-    filename: isProduction ? '[name].[contenthash].js' : '[name].js',
-    publicPath: '/'
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   },
-  mode: isProduction ? 'production' : 'development',
-  devtool: isProduction ? 'source-map' : 'eval-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-          'css-loader',
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        type: 'asset/resource'
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './src/index.js',
+      output: {
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash].[ext]'
       }
-    ]
+    }
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      minify: isProduction
-    }),
-    new MiniCssExtractPlugin({
-      filename: isProduction ? '[name].[contenthash].css' : '[name].css'
-    }),
-    new DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
-  ],
-  devServer: {
-    contentBase: path.resolve('dist'),
-    compress: true,
-    port: 9000,
-    historyApiFallback: true,
-    open: true
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    },
-    runtimeChunk: 'single'
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
-};
+});
