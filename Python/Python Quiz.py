c23fa2b99cc7395caa612966c3ad1f3d3b3d00bb
SkandaBT <9980056379Skanda@gmail.com>
2024-08-09 19:41:25 +0530

    Update Python Quiz.py
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- Python/Python Quiz.py
+++ Python/Python Quiz.py
@@ -3,23 +3,42 @@ answer = input('Are you ready to play the Quiz? (yes/no): ')
 
 if answer.lower() == 'yes':
     score = 0
-    total_questions = 3
+    total_questions = 5
 
-    answer = input('Question 1: What is your favorite programming language? ')
-    if answer.lower() == 'python':
+    answer = input('Question 1: What is 5 + 7? ')
+    if answer == '12':
         score += 1
         print('Correct!')
     else:
         print('Wrong answer!')
 
-    answer = input('Question 2: Who played football in Real Madrid in the 2014 season?')
-    if answer.lower() == 'C.Ronaldo':
+    answer = input('Question 2: Who played football in Real Madrid in the 2014 season? ')
+    if answer.lower() == 'Ronaldo':
         score += 1
         print('Correct!')
     else:
         print('Wrong answer!')
-  
-  # Add more questions here...
+
+    answer = input('Question 3: What is the square root of 64? ')
+    if answer == '8':
+        score += 1
+        print('Correct!')
+    else:
+        print('Wrong answer!')
+
+    answer = input('Question 4: What is 15 * 3? ')
+    if answer == '45':
+        score += 1
+        print('Correct!')
+    else:
+        print('Wrong answer!')
+
+    answer = input('Question 5: What is 100 divided by 4? ')
+    if answer == '25':
+        score += 1
+        print('Correct!')
+    else:
+        print('Wrong answer!')
 
     print(f'Thank you for playing! You attempted {score} questions correctly.')
     mark = (score / total_questions) * 100
