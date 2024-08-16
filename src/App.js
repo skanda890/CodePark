0863fddb10d2f3eea1da0992efa343bde585ef77
SkandaBT <9980056379Skanda@gmail.com>
2024-08-16 12:40:37 +0530

    Create App.js
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to CodePark</h1>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
