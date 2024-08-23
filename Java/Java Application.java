83e50e965403b06a357ed04fe42af30e0858f94d
SkandaBT <9980056379Skanda@gmail.com>
2024-08-23 16:03:32 +0530

    Update Java Application.java
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- Java/Java Application.java
+++ Java/Java Application.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class FactorialCalculator {
-    public static void main(String[] args) {
+public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a positive integer: ");
         int num = scanner.nextInt();
