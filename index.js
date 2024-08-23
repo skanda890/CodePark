66d09c8ca13622fc481aec4c6e1ce2c9cf32e2ca
SkandaBT <9980056379Skanda@gmail.com>
2024-08-23 22:16:00 +0530

    Update index.js
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- index.js
+++ index.js
@@ -3,7 +3,29 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`
+    <html>
+      <head>
+        <style>
+          .container {
+            height: 100vh;
+            width: 100vw;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+          .message {
+            font-size: 24px;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <div class="message">Hello World!</div>
+        </div>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(port, () => {
