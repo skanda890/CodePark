7537ed1e86ba42bf371aa45d8084791adc9437b4
SkandaBT <9980056379Skanda@gmail.com>
2024-08-23 22:20:46 +0530

    Revert "Update index.js (#72)"
    This reverts commit 97b621cf4084cf7401ad317c970f2c4c4704f43f.

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ app.get('/', (req, res) => {
       <head>
         <style>
           .container {
-            height: 100000vh;
-            width: 100000vw;
+            height: 100vh;
+            width: 100vw;
             display: flex;
             justify-content: center;
             align-items: center;
           }
           .message {
-            font-size: 999999999px;
+            font-size: 24px;
           }
         </style>
       </head>
