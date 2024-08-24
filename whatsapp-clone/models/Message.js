3eb61841323addf2d65183a1be613fb6e966a995
Skanda890 <99239826+skanda890@users.noreply.github.com>
2024-08-24 22:03:34 +0530

    Flush

--- whatsapp-clone/models/Message.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const messageSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    content: { type: String, required: true },
-    timestamp: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Message', messageSchema);
