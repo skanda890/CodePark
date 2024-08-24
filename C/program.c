e284bfb7e1fba408192b0609e58d9ae413985056
SkandaBT <9980056379Skanda@gmail.com>
2024-08-24 10:24:43 +0530

    Create program.c
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

---
+++ C/program.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void playGame() {
+    char *choices[] = {"Rock", "Paper", "Scissors"};
+    int userChoice, computerChoice;
+
+    printf("Choose an option:\n");
+    printf("1. Rock\n2. Paper\n3. Scissors\n");
+    printf("Enter your choice: ");
+    scanf("%d", &userChoice);
+
+    if (userChoice < 1 || userChoice > 3) {
+        printf("Invalid choice! Please choose a number between 1 and 3.\n");
+        return;
+    }
+
+    srand(time(0));
+    computerChoice = rand() % 3 + 1;
+
+    printf("You chose: %s\n", choices[userChoice - 1]);
+    printf("Computer chose: %s\n", choices[computerChoice - 1]);
+
+    if (userChoice == computerChoice) {
+        printf("It's a tie!\n");
+    } else if ((userChoice == 1 && computerChoice == 3) ||
+               (userChoice == 2 && computerChoice == 1) ||
+               (userChoice == 3 && computerChoice == 2)) {
+        printf("You win!\n");
+    } else {
+        printf("You lose!\n");
+    }
+}
+
+int main() {
+    playGame();
+    return 0;
+}
