9d829da59b2052e7a49e4bea75276b5a15785fb3
Skanda890 <99239826+skanda890@users.noreply.github.com>
2024-08-26 17:41:56 +0530

    Revert2

--- my-express-app/controllers/userController.js
+++
@@ -1,7 +0,0 @@
-exports.getUsers = (req, res) => {
-  res.send('User list');
-};
-
-exports.createUser = (req, res) => {
-  res.send('Create user');
-};
--- my-express-app/models/userModels.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  age: { type: Number, required: true },
-});
-
-module.exports = mongoose.model('User', userSchema);
