e75795e10e4ab498e293f9cf434af8273ace5ab0
SkandaBT <9980056379Skanda@gmail.com>
2024-08-28 20:38:46 +0530

    Update start-vm.js
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- hyper-v-npm-project/start-vm.js
+++ hyper-v-npm-project/start-vm.js
@@ -1,5 +1,22 @@
-const ps = new (require('node-powershell'))();
-ps.addCommand('Start-VM -Name MyVM');
-ps.invoke()
-  .then(() => console.log('VM started successfully!'))
-  .catch(err => console.error('Error starting VM:', err));
+const ps = require('node-powershell');
+
+async function startVM() {
+  const vmName = 'MyVM'; // Replace with your VM name
+
+  const psInstance = new ps({
+    executionPolicy: 'Bypass',
+    noProfile: true,
+  });
+
+  try {
+    await psInstance.addCommand(`Start-VM -Name "${vmName}"`);
+    await psInstance.invoke();
+    console.log(`VM "${vmName}" started successfully!`);
+  } catch (err) {
+    console.error('Error starting VM:', err);
+  } finally {
+    await psInstance.dispose();
+  }
+}
+
+startVM();
