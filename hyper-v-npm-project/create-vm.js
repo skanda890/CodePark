b24018cad720609888aca4af205930e9fcb6dd91
SkandaBT <9980056379Skanda@gmail.com>
2024-08-28 20:34:01 +0530

    Update create-vm.js
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- hyper-v-npm-project/create-vm.js
+++ hyper-v-npm-project/create-vm.js
@@ -1,1 +1,5 @@
-
+const ps = new (require('node-powershell'))();
+ps.addCommand('New-VM -Name MyVM -MemoryStartupBytes 2GB -NewVHDPath "A:\\VMs\\MyVM.vhdx" -NewVHDSizeBytes 5000GB');
+ps.invoke()
+  .then(() => console.log('VM created successfully!'))
+  .catch(err => console.error('Error creating VM:', err));
