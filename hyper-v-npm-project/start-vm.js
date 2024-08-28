f0e1d333cc2d901e3a9738b892067107233f8c87
SkandaBT <9980056379Skanda@gmail.com>
2024-08-28 20:34:17 +0530

    Update start-vm.js
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- hyper-v-npm-project/start-vm.js
+++ hyper-v-npm-project/start-vm.js
@@ -1,1 +1,5 @@
-
+const ps = new (require('node-powershell'))();
+ps.addCommand('Start-VM -Name MyVM');
+ps.invoke()
+  .then(() => console.log('VM started successfully!'))
+  .catch(err => console.error('Error starting VM:', err));
