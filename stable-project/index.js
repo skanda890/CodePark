efb0ca4c92592eae4a18a7944e4a54e3388c88de
SkandaBT <9980056379Skanda@gmail.com>
2024-08-28 20:52:54 +0530

    Update index.js
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- stable-project/index.js
+++ stable-project/index.js
@@ -1,4 +1,14 @@
-const _ = require('lodash');
+// index.js
+const express = require('express');
+const app = express();
+const port = 3001;
 
-const greeting = _.capitalize('hello, world');
-console.log(greeting);
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
