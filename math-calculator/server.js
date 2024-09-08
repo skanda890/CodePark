4b902235cf52d8ba21adb8ce704f185ab3799015
SkandaBT <9980056379Skanda@gmail.com>
2024-09-08 16:31:55 +0530

    Update server.js
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- math-calculator/server.js
+++ math-calculator/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const math = require('mathjs');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
