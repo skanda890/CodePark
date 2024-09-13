e2192a42edc7b751223b20ce57dc961d79dc7abc
SkandaBT <9980056379Skanda@gmail.com>
2024-09-13 21:55:19 +0530

    Update server.js
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- my-portfolio/server.js
+++ my-portfolio/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4001;
 
 app.get('/', (req, res) => {
   res.send('Welcome to My Portfolio!');
