1bd99cc670ac405736ed2c8ced0e21ebf5e36414
SkandaBT <9980056379Skanda@gmail.com>
2024-09-17 17:29:09 +0530

    Delete Sudoku-Puzzles directory
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- Sudoku-Puzzles/node_modules/sudoku-gen/dist/constants/grid-size.constant.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GRID_SIZE = void 0;
-exports.GRID_SIZE = 9;
--- Sudoku-Puzzles/node_modules/sudoku-gen/dist/types/difficulty.type.d.ts
+++
@@ -1,1 +0,0 @@
-export declare type Difficulty = 'easy' | 'medium' | 'hard' | 'expert';
