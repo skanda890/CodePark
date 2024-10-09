dd327fd83fd5e53176c71b941a9c0322afc42416
SkandaBT <9980056379Skanda@gmail.com>
2024-10-09 10:18:32 +0530

    Create index.js
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

---
+++ youtube-downloader/index.js
@@ -0,0 +1,21 @@
+const ytdl = require('ytdl-core');
+const readlineSync = require('readline-sync');
+const fs = require('fs');
+
+const videoUrl = readlineSync.question('Enter the YouTube video URL: ');
+
+if (ytdl.validateURL(videoUrl)) {
+  const videoId = ytdl.getURLVideoID(videoUrl);
+  const output = `${videoId}.mp4`;
+
+  ytdl(videoUrl)
+    .pipe(fs.createWriteStream(output))
+    .on('finish', () => {
+      console.log(`Video downloaded successfully as ${output}`);
+    })
+    .on('error', (err) => {
+      console.error('Error downloading video:', err);
+    });
+} else {
+  console.log('Invalid YouTube URL. Please try again.');
+}
