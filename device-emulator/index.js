b5c0fede6e2dd9716526c6c95e2787d55de62d96
SkandaBT <9980056379Skanda@gmail.com>
2024-10-09 12:41:37 +0530

    Update index.js
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- device-emulator/index.js
+++ device-emulator/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+
+// Serve the index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Emulate a device
 app.post('/emulate', (req, res) => {
@@ -20,5 +26,5 @@ app.post('/emulate', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Device emulator running on port ${PORT}`);
+    console.log(`DeviceEmulator running on port ${PORT}`);
 });
