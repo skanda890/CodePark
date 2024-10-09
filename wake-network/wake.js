401d898c161b99a7b504d96d8a4155605e6ee7a7
SkandaBT <9980056379Skanda@gmail.com>
2024-10-09 11:13:59 +0530

    Update wake.js
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- wake-network/wake.js
+++ wake-network/wake.js
@@ -5,6 +5,8 @@ const localDevices = require('local-devices');
 async function wakeAllDevices() {
     try {
         const devices = await localDevices();
+        let pendingWakeSignals = devices.length;
+
         devices.forEach(device => {
             wol.wake(device.mac, function(error) {
                 if (error) {
@@ -12,8 +14,14 @@ async function wakeAllDevices() {
                 } else {
                     console.log(`Sent wake signal to ${device.name} (${device.mac})`);
                 }
+
+                pendingWakeSignals -= 1;
+                if (pendingWakeSignals === 0) {
+                    console.log('All wake signals sent.');
+                }
             });
         });
+
     } catch (error) {
         console.error('Error fetching devices:', error);
     }
