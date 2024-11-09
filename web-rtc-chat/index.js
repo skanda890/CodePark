d0ab0a5fea063f6d1a2b23691720c09455a2e6b2
SkandaBT <9980056379Skanda@gmail.com>
2024-11-09 12:09:18 +0530

    Create index.js
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

---
+++ web-rtc-chat/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const CryptoJS = require('crypto-js');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('offer', (offer) => {
+    socket.broadcast.emit('offer', offer);
+  });
+
+  socket.on('answer', (answer) => {
+    socket.broadcast.emit('answer', answer);
+  });
+
+  socket.on('ice-candidate', (candidate) => {
+    socket.broadcast.emit('ice-candidate', candidate);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
