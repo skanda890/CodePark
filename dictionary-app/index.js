dea841567aefaa4b3272baf691122fd7883750d7
SkandaBT <9980056379Skanda@gmail.com>
2024-11-23 09:52:52 +0530

    Update index.js
    Signed-off-by: SkandaBT <9980056379Skanda@gmail.com>

--- dictionary-app/index.js
+++ dictionary-app/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 5000;
 
-const API_URL = 'https://api.freedictionary.dev/api/v2/entries/en/';
+const API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
