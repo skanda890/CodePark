81bf00ef64094ef6b8c8faa4f0d167c0b61f56f4
SkandaBT <9980056379Skanda@gmail.com>
2025-05-02 11:29:55 +0530

    Edit index.js

--- windows-insider-blogs/index.js
+++ windows-insider-blogs/index.js
@@ -8,7 +8,7 @@ const parser = new Parser();
 const FEED_URL = "https://blogs.windows.com/feed/"; // Windows Insider Blog Feed
 
 async function fetchBlogs(limit = 5) {
-  console.log(chalk.blue("\nFetching Windows Insider blog posts...\n"));
+  console.log(`\n${chalk.blue("Fetching Windows Insider blog posts...")}\n`);
   
   try {
     const feed = await parser.parseURL(FEED_URL);
